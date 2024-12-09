25ab93536721503ae319fb7ab48e5233ac8ba85a
a1exx2a <a1exx2a@yandex.ru>
2024-12-09 17:27:09 +0300

    init commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Task9;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] x = {2,5,9,4,13,6};
+        Array.Sort(x);
+
+        foreach (int i in x){
+            Console.WriteLine(i);
+        }
+    }
+}
